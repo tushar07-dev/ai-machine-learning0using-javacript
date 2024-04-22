060471b1ce49129a889245fbf2464937f0282bed
tushar shelke <shelketushar100@gmail.com>
2024-04-22 17:03:58 +0530

    keyclock is npt working

--- realtime-code-whiteboard/my-whiteboard/src/App.tsx
+++ realtime-code-whiteboard/my-whiteboard/src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import { KeycloakProvider } from '@react-keycloak/web';
 import keycloak from './keycloak';
-import Login from '../components/Login';
-import Signup from '../components/Signup';
-import Whiteboard from '../components/Whiteboard';
+import Login from '../src/components/Login';
+import Signup from '../src/components/Signup';
+import Whiteboard from '../src/components/Whiteboard';
 
 const App: React.FC = () => {
   return (
